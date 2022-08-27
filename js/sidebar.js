97b604de16ebf97919dea51665b9a6d05133ed20
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-08-27 21:29:35 +0200

    trouble with watching sass

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,10 +1,13 @@
 const sidebar = document.querySelector('.sidebar');
 const postsContainer = document.querySelector('.posts-container');
 const sidebarWrapper = document.querySelector('.sidebar-wrapper');
-// console.log(sidebar.getBoundingClientRect().width);
 
 window.addEventListener('resize', (e) => {
-  const sidebarWidth = sidebar.getBoundingClientRect().width;
-  console.log(sidebarWidth);
-  //   postsContainer.style.marginRight = '300px';
+  removeSidebarWidth();
 });
+
+function removeSidebarWidth() {
+  const sidebarWidth = sidebar.getBoundingClientRect().width;
+  postsContainer.style.marginLeft = `${sidebarWidth}px`;
+}
+removeSidebarWidth();
