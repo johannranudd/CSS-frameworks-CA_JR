b8af1cec377f00e2374ae7ed0ed1c10b80948a1e
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-08-28 02:32:04 +0200

    image uploads but is not responsive yet

--- js/sidebar.js
+++ js/sidebar.js
@@ -11,3 +11,59 @@ function removeSidebarWidth() {
   postsContainer.style.marginLeft = `${sidebarWidth}px`;
 }
 removeSidebarWidth();
+
+// test
+// const imgBtn = document.querySelector('.image-btn');
+const imgInput = document.querySelector('#image-input');
+const imgDisplayer = document.querySelector('.image-displayer');
+
+imgInput.addEventListener('change', (e) => {
+  // console.log(imgInput.files);
+  const reader = new FileReader();
+  reader.onload = function () {
+    const img = new Image();
+    img.src = reader.result;
+    imgDisplayer.appendChild(img);
+  };
+  reader.readAsDataURL(imgInput.files[0]);
+});
+
+//
+//
+//
+//
+//
+//
+//
+//
+
+// let uploadedImage = '';
+// imgInput.addEventListener('change', () => {
+//   // console.log(imgInput.value);
+//   const reader = new FileReader();
+//   reader.addEventListener('load', () => {
+//     uploadedImage = reader.result;
+//     imgDisplayer.style.backgroundImage = `url(${uploadedImage})`;
+//   });
+//   reader.readAsDataURL(this.files[0]);
+// });
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// async function openFile() {
+//   let [fileHandle] = await window.showOpenFilePicker();
+//   let fileData = await fileHandle.getFile();
+//   console.log(fileData);
+// }
+
+// imgBtn.addEventListener('click', () => {
+//   openFile();
+// });
