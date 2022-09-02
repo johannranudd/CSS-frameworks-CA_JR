65985cf36e04c2981bd93ce1baa88f4a41379988
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-09-02 06:11:45 +0200

    form validation

---
+++ js/login.js
@@ -0,0 +1,17 @@
+const emailRegex = RegExp(/^\S+@\S+\.\S+$/);
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const alert = document.querySelector('.warning');
+
+loginForm.addEventListener('submit', (e) => {
+  const eResult = emailRegex.test(emailInput.value);
+  if (!eResult || passwordInput.value.length < 6) {
+    e.preventDefault();
+    alert.innerHTML =
+      'Email address must be valid and password must contain at least 6 characters';
+    alert.style.backgroundColor = 'red';
+    alert.style.color = 'white';
+    alert.style.padding = '1rem';
+  }
+});
